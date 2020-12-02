16b0a3789caad01f7e491eb85ecfa23902056cac
sgir <sathya.prabha.girish@sap.com>
2020-12-01 20:56:22 -0500

    part-1 : two sum binary search

--- Day1/sumto2020.js
+++ Day1/sumto2020.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const readLine = require('readline');
-
+let arrayInput;
 //1. read input into an array
 
 // don't do this - won't work for big files as it reads all into memory
@@ -8,7 +8,7 @@ const readLine = require('readline');
 async function processLineByLine (){
     let input = [];
     const readInterface = readLine.createInterface({
-        input: fs.createReadStream('/Users/i850773/Developer/poc/AdventofCode/Day1/input.txt'),
+        input: fs.createReadStream('/Users/i850773/Developer/poc/AdventofCode/Day1/input_1.txt'),
         output: process.stdout,
         crlfDelay: Infinity,
         console: false
@@ -23,7 +23,7 @@ async function processLineByLine (){
 // 2. sort input
 
 async function sortInput(){
-    let arrayInput = await processLineByLine();
+    arrayInput = await processLineByLine();
     console.log("Read from File",arrayInput);
     console.log("Read from File",arrayInput.length);
     sortedInput = arrayInput.sort((a,b)=>{
@@ -31,22 +31,68 @@ async function sortInput(){
     });
     return sortedInput;
 }
-let sortedInput = sortInput().then((sortedInput) => {
-    console.log("Sorted Input",sortedInput);
-    console.log("Sorted Input",sortedInput.length);
-});
+
 
 
 //3. Binary Search
-
-
-//4. return product
-
 /**
  * @param {number[]} nums
  * @param {number} target
- * @return {number[]}
+ * @return {number}
  */
-// var twoSum = async function(nums, target) {
-    
-// };
+
+async function findSum(sortedInput,sum){
+    arrayInput = await processLineByLine();
+    for(let i=0;i<arrayInput.length;i++){
+        console.log("Searching a match for", arrayInput[i])
+        let val = binarySearch(sortedInput,sum,i);
+        console.log(val);
+        if(!val){
+            continue
+        } else {
+            return val;
+        }
+    }
+}
+
+// Need to sort for Binary Search
+let sortedInput = sortInput().then((sortedInput) => {
+    console.log("Sorted Input",sortedInput);
+    console.log("Sorted Input",sortedInput.length);
+    console.log(findSum(sortedInput,2020)) // function(nums, target)
+});
+
+
+var binarySearch = function(nums, target,i) {
+    let len, m, mid;
+
+    // 1. get the number of items
+    len = nums.length;
+    console.log('1. get the number of items',len)
+    if(len==0){
+        return
+    }
+
+    // 2. find the midpoint index and element
+    m = Math.ceil((len+1)/2)
+    mid = nums[m-1];
+    console.log(`2. find the midpoint index ${m-1} and middle element ${mid}`)
+
+    console.log(`3. Searching for`, target-arrayInput[i])
+
+    // 3. do the check
+    if(target-arrayInput[i]<mid){ //LHS tree
+        console.log('LHS')
+        binarySearch(nums.slice(0,m-1),target,i)
+
+    } else if(target-arrayInput[i]>mid) { //RHS tree
+        console.log('RHS')
+        binarySearch(nums.slice(m,len),target,i)
+    }
+    else if(mid==target-arrayInput[i]){
+        console.log('equal')
+        return mid;
+    } else { //no match
+        return
+    }
+};
